9d9227135d77271268e10e9bea39bdba896a4855
David Barcenas <daveepro@outlook.com>
2020-08-24 15:48:59 -0500

    style: Create registration and login layout

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
   return (
-    <div>
-      <h1>LoginScreen</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Login</h3>
+
+      <form>
+        <input type="email" className="auth__input" placeholder="Email" name="mail"/>
+        <input type="password" className="auth__input" placeholder="Password" name="pwd"/>
+        <button type="submit" className="btn btn-primary btn-block">Sign in</button>
+      </form>
+
+      <div>
+        <span className="auth__social-text">Login with social networks</span>
+        <button className="btn btn-block btn__google" >
+          <div className="btn__google-img" >
+            <img src="/images/google.svg" alt="google"/>
+          </div>
+          <span>Sign in with google</span>
+        </button>
+
+        <Link to="/auth/register" className="auth__social-text"> Create new account </Link>
+      </div>
+    </>
   )
 }
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>RegisterScreen</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Register</h3>
+
+      <form>
+        <input type="text" className="auth__input" placeholder="Name" name="name"/>
+        <input type="email" className="auth__input" placeholder="Email" name="mail"/>
+        <input type="password" className="auth__input" placeholder="Password" name="pwd"/>
+        <input type="password" className="auth__input" placeholder="Password confirm" name="pwdConfirm"/>
+        <button type="submit" className="btn btn-primary btn-block">Register</button>
+      </form>
+
+      <Link to="/auth/login" className="auth__social-text"> Already registered?</Link>
+    </>
   )
 }
