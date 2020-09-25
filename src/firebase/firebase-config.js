f700481643915ff1521ad0098db575f4fd9143a6
David Barcenas <daveepro@outlook.com>
2020-09-25 17:39:36 -0500

    feat: ENVIRONMENTS

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,18 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyASgxEcbrc-OBUwkKpvf9-bJS9s_OG8O6k",
-  authDomain: "react-redux-49413.firebaseapp.com",
-  databaseURL: "https://react-redux-49413.firebaseio.com",
-  projectId: "react-redux-49413",
-  storageBucket: "react-redux-49413.appspot.com",
-  messagingSenderId: "197706763821",
-  appId: "1:197706763821:web:f6838ac75860f42ed372a4"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL:process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 export const firebaseConfigTesting = {}
-
-/*if( process.env.NODE_ENV === 'test' ) {
-
-} else {
-  // dev / prod
-}
-*/
