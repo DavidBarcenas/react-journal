be08d8f423b71c91a5cb5838c9273361b92d3961
David Barcenas <daveepro@outlook.com>
2020-09-25 19:22:20 -0500

    test: Notes actions [toMatchObject]

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,20 +1,26 @@
 import '@testing-library/jest-dom';
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk';
-import { startNewNote } from '../../actions/notes';
+import { startLoadingNotes, startNewNote, startSaveNote } from '../../actions/notes';
 import { db } from '../../firebase/firebase-provider';
 import { types } from '../../types/types';
  
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
-const store = mockStore({
+const initState = {
   auth: {
     uid: 'uTesiing'
   }
-})
+}
+
+let store = mockStore( initState )
 
 describe('<notes-actions /> testing', () => {
+
+  beforeEach(() => {
+    store = mockStore( initState )
+  })
 
   test('should create a new note', async() => {
     await store.dispatch( startNewNote() )
@@ -43,6 +49,39 @@ describe('<notes-actions /> testing', () => {
     // delete record firebase
     const docId = actions[0].payload.id;
     await db.doc(`/TESTING/journal/notes/${docId}`).delete()
+  })
+
+  test('should load notes', async () => {
+    await store.dispatch( startLoadingNotes('journal') )
+    const actions = store.getActions()
+
+    expect( actions[0] ).toEqual({
+      type: types.noteLoad,
+      payload: expect.any(Array)
+    })
+
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number)
+    }
+
+    expect( actions[0].payload[0] ).toMatchObject( expected )
+  })
+  
+  
+  test('should update the note', async () => {
+    const note = {
+      id: '231asdas65d4as8d7as',
+      title: 'title',
+      body: testing
+    }
+
+    await store.dispatch(startSaveNote( note ))
+
+    const actions = store.getActions()
+    expect( actions[0].type ).toBe(types.noteUpdated)
   })
   
 })
